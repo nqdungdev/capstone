356674d7cbbf7506d1fab9a67bccf8a24e397a4a
nqdungdev <nqdungdev@gmail.com>
2022-04-18 18:51:50 +0700

    header-v1.2

--- js/script.js
+++ js/script.js
@@ -9,21 +9,14 @@ checkBox.addEventListener('click', function () {
         //    changeTheme.attributes['style'].textContent='background-color: #100f19;' 
            
         // }
-        var bgColors = document.querySelectorAll('.bg--white')
-        for (var bgColor of bgColors) {
-            bgColor.classList.remove('bg--white')
-            bgColor.classList.add('bg--dark')
-        }
-        var textColors = document.querySelectorAll('.title--day')
-        for (var textColor of textColors) {
-            textColor.classList.remove('title--day')
-            textColor.classList.add('title--night')
-        }
-        var textBodyColors = document.querySelectorAll('.text-body--day')
-        for (var textBodyColor of textBodyColors) {
-            textBodyColor.classList.remove('text-body--day')
-            textBodyColor.classList.add('text-body--night')
-        }
+        // var bgColors = document.querySelectorAll('.bg--white')
+        // for (var bgColor of bgColors) {
+        //     bgColor.classList.remove('bg--white')
+        //     bgColor.classList.add('bg--dark')
+        // }
+        changeBg("bg--white","bg--dark")
+        changeText("title--day","title--night")
+        changeText("text-body--day","text-body--night")
     } else {
         document.getElementById('js-moon').style.display = "block"
         document.getElementById('js-sun').style.display = "none"
@@ -32,30 +25,71 @@ checkBox.addEventListener('click', function () {
         //    if(changeTheme.attributes['style'].textContent === "background-color: #100f19;")
         //    changeTheme.attributes['style'].textContent='background-color: #fff;'            
         // }
-        var bgColors = document.querySelectorAll('.bg--dark')
-        for (var bgColor of bgColors) {
-            bgColor.classList.remove('bg--dark')
-            bgColor.classList.add('bg--white')
-        }
-        var textColors = document.querySelectorAll('.title--night')
-        for (var textColor of textColors) {
-            textColor.classList.remove('title--night')
-            textColor.classList.add('title--day')
-        }
-        var textBodyColors = document.querySelectorAll('.text-body--night')
-        for (var textBodyColor of textBodyColors) {
-            textBodyColor.classList.remove('text-body--night')
-            textBodyColor.classList.add('text-body--day')
-        }
+        // var bgColors = document.querySelectorAll('.bg--dark')
+        // for (var bgColor of bgColors) {
+        //     bgColor.classList.remove('bg--dark')
+        //     bgColor.classList.add('bg--white')
+        // }
+        changeBg("bg--dark","bg--white")
+        changeText("title--night","title--day")
+        changeText("text-body--night","text-body--day")
     }
 });
 
-const changeTheme = (firstClass,nextClass) => {
-    console.log(firstClass,nextClass,`'.${firstClass}'`);
-    var colors = document.querySelectorAll(`'.${firstClass}'`)
-    console.log(colors);
-        for (var color of colors) {
-            color.classList.remove(`'${firstClass}'`)
-            color.classList.add(`'${nextClass}'`)
+const changeBg = (prevClass,nextClass) => {
+    var bgColors = document.querySelectorAll(`.${prevClass}`)
+    console.log(bgColors);
+        for (var bgColor of bgColors) {
+            console.log(bgColor);
+            bgColor.classList.remove(`${prevClass}`)
+            bgColor.classList.add(`${nextClass}`)
         }
 }
+const changeText = (prevClass,nextClass) => {
+    var colors = document.querySelectorAll(`.${prevClass}`)
+    console.log(colors);
+        for (var color of colors) {
+            console.log(color);
+            color.classList.remove(`${prevClass}`)
+            color.classList.add(`${nextClass}`)
+        }
+}
+
+
+window.addEventListener("scroll", function () {
+    var headerBg = this.document.getElementById('header');
+    var headerTitles = this.document.getElementsByClassName('header__title');
+  if(pageYOffset>=50){
+       headerBg.style.height="8rem";
+      if(!checkBox.checked){
+        headerBg.classList.add('bg--white');
+        for(var headerTitle of headerTitles){
+        headerTitle.classList.remove('title--night')
+        headerTitle.classList.add('title--day')}
+      } else {
+        headerBg.classList.add('bg--dark');
+        for(var headerTitle of headerTitles){
+        headerTitle.classList.remove('title--day')
+        headerTitle.classList.add('title--night')
+    }
+      }   
+     
+  } else if(pageYOffset<50){
+          headerBg.classList.remove('bg--white');
+          headerBg.classList.remove('bg--dark');
+        headerBg.style.height="10rem";
+        // headerBg.style.backgroundColor = "transparent";
+    for(var headerTitle of headerTitles){
+        headerTitle.classList.remove('title--day')
+        headerTitle.classList.add('title--night')}
+    
+  }
+  
+//   document.getElementById("translate-1").style.transform =
+//     "translate3d(0, " + `${x * 0.14}px` + ", 0)";
+//   document.getElementById("translate-2").style.transform =
+//     "translate3d(0, " + `${x * 0.2}px` + ", 0)";
+//   document.getElementById("translate-3").style.transform =
+//     "translate3d(0, " + `${x * -0.2}px` + ", 0)";
+});
+
